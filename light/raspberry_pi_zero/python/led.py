95046a4f0ecf00ad6add83594a1e40f21300910b
Mike Barkmin <mike.barkmin@uni-due.de>
2019-08-06 16:10:48 +0200

    Add raspberry pi zero light

---
+++ light/raspberry_pi_zero/python/led.py
@@ -0,0 +1,48 @@
+from gpiozero import PWMLED
+
+
+class Led():
+    def __init__(self, r: float, g: float, b: float):
+        self.__r = r
+        self.__r_pwm = PWMLED(2)
+
+        self.__g = g
+        self.__g_pwm = PWMLED(3)
+
+        self.__b = b
+        self.__b_pwm = PWMLED(4)
+
+    @classmethod
+    def limit_to_rgb(cls, number):
+        if number < 0:
+            return 0
+        if number > 255:
+            return 255
+        return number
+
+    @property
+    def r(self) -> float:
+        return self.__r
+
+    @r.setter
+    def r(self, r: float):
+        self.__r = Led.limit_to_rgb(r)
+        self.__r_pwm.value = self.__r / 255.0
+
+    @property
+    def g(self) -> float:
+        return self.__g
+
+    @g.setter
+    def g(self, g: float):
+        self.__g = Led.limit_to_rgb(g)
+        self.__g_pwm.value = self.__g / 255.0
+
+    @property
+    def b(self) -> float:
+        return self.__b
+
+    @b.setter
+    def b(self, b: float):
+        self.__b = Led.limit_to_rgb(b)
+        self.__b_pwm.value = self.__b / 255.0
---
+++ light/raspberry_pi_zero/python/server.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+from led import Led
+
+app = Flask(__name__)
+
+led = Led(0, 0, 0)
+
+
+@app.route("/led", methods=["GET", "POST"])
+def handle_led():
+    if request.method == "GET":
+        return get_led()
+    elif request.method == "POST":
+        return post_led()
+
+
+def get_led():
+    return jsonify({"r": led.r, "g": led.g, "b": led.b})
+
+
+def post_led():
+    json = request.get_json()
+
+    led.r = json.get("r", led.r)
+    led.g = json.get("g", led.g)
+    led.b = json.get("b", led.b)
+
+    return jsonify({"message": "success"})
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000, debug=True)
